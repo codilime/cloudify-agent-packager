86bc7d30184f594afe9896960ee2adfa5561fdd1
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2016-06-07 16:47:10 +0200

    CFY-4911 asd

--- agent_packager/packager.py
+++ agent_packager/packager.py
@@ -24,8 +24,8 @@ CORE_MODULES_LIST = [
 ]
 
 CORE_PLUGINS_LIST = [
-    'cloudify_script_plugin',
-    'cloudify_diamond_plugin',
+    # 'cloudify_script_plugin',
+    # 'cloudify_diamond_plugin',
 ]
 
 MANDATORY_MODULES = [
